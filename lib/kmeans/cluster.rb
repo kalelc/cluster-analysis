0820e5d4a64734b7941dc19c5d361748b2cef1da
Andres Colonia <andreskal@gmail.com>
2016-10-26 14:25:56 -0300

    add class to create clusters with data and centroid

---
+++ lib/kmeans/cluster.rb
@@ -0,0 +1,29 @@
+module Kmeans
+  class Cluster
+    attr_accessor :centroid, :points
+
+    def initialize(centroid)
+      @centroid = centroid
+      @points = []
+    end
+
+    def centroid!
+      xa = ya = 0
+      old_centroid = @centroid
+
+      @points.each do |point|
+        xa += point.x
+        ya += point.y
+      end
+
+      if (total = @points.length) != 0
+        xa /= total
+        ya /= total
+        @centroid = Point.new(xa, ya)
+        @points = []
+      end
+
+      return GpsWaypoints.distance(old_centroid.to_a,centroid.to_a)
+    end
+  end
+end
