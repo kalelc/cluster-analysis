a17787ac0c32033acbbb281f7de4ac00725bfe11
Andres Colonia <andreskal@gmail.com>
2016-10-26 14:26:28 -0300

    kmeans implementation

---
+++ lib/kmeans/kmeans.rb
@@ -0,0 +1,46 @@
+module Kmeans
+  class Kmeans
+    attr_reader :data, :k, :clusters
+
+    def initialize(data, k, iteration = 10)
+      @data = data
+      @k = k
+      @iteration = iteration
+      @clusters = []
+    end
+
+    def assigment_step
+      (1..@k).each do |cluster|
+        index = (@data.length * rand).to_i
+        point = Point.new(@data[index].first, @data[index].last)
+        @clusters << Cluster.new(point)
+      end
+      group_elements
+    end
+
+    def update_step
+      @iteration.times do
+        moved = 0
+        @clusters.each do |c|
+          moved += c.centroid!
+        end
+
+        group_elements
+        break if moved == 0
+      end
+    end
+
+    private
+    def group_elements
+      @data.each do |d|
+        distance = []
+        @clusters.each_with_index do |cluster, index|
+          distance[index] = GpsWaypoints.distance(cluster.centroid.to_a, d)
+        end
+        a, b = distance.each_with_index.min
+        clusters[b].points << Point.new(d.first, d.last)
+      end
+      return @clusters
+    end
+  end
+end
