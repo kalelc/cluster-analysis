9a83b788be99a2ce5b8dd55ed79a524ac8228bb6
Andres Colonia <andreskal@gmail.com>
2016-11-03 12:42:31 -0300

    fix code

--- test.rb
+++ test.rb
@@ -4,10 +4,19 @@ require_relative "lib/plot"
 
 class Test
   def initialize
-    data = [] ; (1..50).each do |i|; data << [Faker::Address.latitude.to_f, Faker::Address.longitude.to_f]; end; k = Kmeans::Kmeans.new(data, 4);
+    data = []
+    (1..10).each do |i|
+      data << [Faker::Address.latitude.to_f, Faker::Address.longitude.to_f];
+    end
+
+    k = Kmeans::Kmeans.new(data, 4)
     k.run
+
     data = k.clusters
-    Plot.new.graph(data)
+    history = k.history
+    history.each_with_index do |h, i|
+      Plot.new.graph(h, i)
+    end
   end
 end
 
