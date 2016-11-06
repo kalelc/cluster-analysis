81cd770a2a2a26f747c28f618b6d77e338c64e37
Andres Colonia <andreskal@gmail.com>
2016-11-06 13:19:23 -0300

    change iterations

--- test.rb
+++ test.rb
@@ -1,22 +1,16 @@
 require "faker"
 require_relative "lib/kmeans/kmeans"
-require_relative "lib/plot"
+require_relative "lib/graph"
 
 class Test
   def initialize
     data = []
-    (1..10).each do |i|
+    (1..100).each do |i|
       data << [Faker::Address.latitude.to_f, Faker::Address.longitude.to_f];
     end
 
-    k = Kmeans::Kmeans.new(data, 4)
+    k = Kmeans::Kmeans.new(data, 10)
     k.run
-
-    data = k.clusters
-    history = k.history
-    history.each_with_index do |h, i|
-      Plot.new.graph(h, i)
-    end
   end
 end
 
