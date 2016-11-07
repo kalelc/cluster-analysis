baa1000ad05f979f68b2db4897b7a5e082fca976
Andres Colonia <andreskal@gmail.com>
2016-11-07 11:20:17 -0300

    set static seed to random values

--- test.rb
+++ test.rb
@@ -5,6 +5,7 @@ require_relative "lib/graph"
 class Test
   def initialize
     data = []
+    srand(10)
     (1..100).each do |i|
       data << [Faker::Address.latitude.to_f, Faker::Address.longitude.to_f];
     end
